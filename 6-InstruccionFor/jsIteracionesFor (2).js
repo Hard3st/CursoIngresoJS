d58864b3be86d896b17f5381874883e651b5952c
Hard3st <60698226+Hard3st@users.noreply.github.com>
2020-02-26 10:49:32 -0300

    clase7

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,11 @@
 function mostrar()
 {
 
+    for(var i=10;i>=1;i--){
+
+        document.write(i+"</br>");
+  
+      }
+  
 
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,15 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var rep = parseInt(prompt("ingrese el número de repeticiones"));
 
+for(i=0;i<rep;i++){
+
+    console.log("Hola UTN FRA");
+
+}
+
+alert("Fin de las repeticiones");
 
 
 }//FIN DE LA FUNCIÓN
