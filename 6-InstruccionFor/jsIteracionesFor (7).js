30aa733c2036c35f89431361c87756db0b922c4d
Hard3st <60698226+Hard3st@users.noreply.github.com>
2020-02-27 12:06:02 -0300

    classF

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,23 @@
 function mostrar()
 {
 
+    var num; 
+    var i;
+    var numDivisores = 0;
 
+    num = parseInt(prompt("Ingrese el numero a dividir: "));
 
+    while(isNaN(num) || num < 1){
+        num = parseInt(prompt("Numero invalido. Ingrese nuevamente un numero: "));
+    }
+
+    for(i=0;i<=num;i++){
+        if(num % i == 0){
+            console.log(i);
+            numDivisores ++;
+        }
+    }
+
+    console.log("La cantidad de divisores son: "+numDivisores);
 
 }//FIN DE LA FUNCIÓN
